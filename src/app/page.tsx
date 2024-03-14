09880087b0839a73430b1d97c6eff1bc9afe8cf1
Ilham Bagas <ilham.s@wistkey.dev>
2024-03-14 14:08:31 +0700

    llint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from "./page.module.css";
 import EChartsGraph from "./component/EChartsGraph";
 
